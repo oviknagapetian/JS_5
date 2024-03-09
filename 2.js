737332d971720496e9215bfa5771868750d05fd9
oviknagapetian <oviknagapetian@gmail.com>
2024-03-09 17:38:24 +0400

    HW5

---
+++ 2.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/*
+Необходимо из объекта post, вывести значения, к которым приписан комментарий, 
+в консоль.
+*/
+
+const post = {
+  author: "John", // вывести этот текст
+  postId: 23,
+  comments: [
+    {
+      userId: 10,
+      userName: "Alex",
+      text: "lorem ipsum",
+      rating: {
+        likes: 10,
+        dislikes: 2, // вывести это число
+      },
+    },
+    {
+      userId: 5, // вывести это число
+      userName: "Jane",
+      text: "lorem ipsum 2", // вывести этот текст
+      rating: {
+        likes: 3,
+        dislikes: 1,
+      },
+    },
+  ],
+};
+
+// Выводим значение свойства "author"
+console.log(post.author);
+
+// Выводим значение свойства "dislikes" из первого комментария
+console.log(post.comments[0].rating.dislikes);
+
+// Выводим значение свойства "userId" из второго комментария
+console.log(post.comments[1].userId);
+
+// Выводим значение свойства "text" из второго комментария
+console.log(post.comments[1].text);
